aad38605c81825eeba0fffdd1309f5a178f2b5dd
cls315 <camilasaya315@gmail.com>
2023-05-27 23:18:33 -0300

    rick and mory backend

---
+++ Server/src/index.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const data = require("./utils/data");
+const PORT = 3001;
+http
+  .createServer((req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    const { url } = req;
+    if (url.includes("/rickandmorty/character")) {
+      const id = url.split("/").at(-1);
+      const character = data.find((character) => character.id === Number(id));
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(character));
+    }
+  })
+  .listen(3001, () => {
+    console.log(`Servidor escuchado en el puerto ${PORT}`);
+  });
